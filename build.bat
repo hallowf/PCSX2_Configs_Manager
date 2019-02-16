87f18f30d8c00b2eec8375abf6dda105836b6474
hallowf <alexandre.cortegaca@gmail.com>
2019-02-16 17:42:30 +0000

    Updated travis config

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 echo upx dir is %UPX_DIR%
 pyinstaller --noconfirm --log-level=WARN ^
+    --upx-dir=R:\Software\UPX ^
     --onefile ^
     --icon="manager.ico" ^
     --name="Manager" ^
     --uac-admin ^
-    main.spec
+    main.py
