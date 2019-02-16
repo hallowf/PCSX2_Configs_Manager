81deac6262ab410c06d3b72757113a469fc27195
hallowf <alexandre.cortegaca@gmail.com>
2019-02-16 18:44:40 +0000

    script for creating package with pyinstaller

---
+++ pyinstaller.py
@@ -0,0 +1,16 @@
+import PyInstaller.__main__
+import os
+
+UPX_PATH = os.environ["UPX_PATH"]
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+PyInstaller.__main__.run([
+        '--noconfirm',
+        '--upx-dir=%s' % (UPX_PATH),
+        '--log-level=WARN',
+        '--onefile',
+        '--name=Manager',
+        '--icon=manager.ico',
+        '--uac-admin',
+        "main.py",
+    ])
