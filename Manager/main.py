bd38379502c9a854d8f3735dea470d39b85a1bb2
hallowf <alexandre.cortegaca@gmail.com>
2019-02-26 00:06:44 +0000

    Added functionality to resume game state

--- Manager/main.py
+++ Manager/main.py
@@ -1,6 +1,6 @@
 import os, sys, configparser, argparse ,subprocess, traceback, shutil, logging, time
 
-from manager_utils import get_manage_option, envs_to_dict, add_args
+from manager_utils import get_manage_option, envs_to_dict, add_args, state_runner
 from manager_classes import GameManager
 
 ## main function
@@ -11,7 +11,12 @@ def run_main(args, config, logger):
     elif " " in  c_game:
         c_game = c_game.split(" ")
     env_dict = envs_to_dict(config, logger)
+    g_manager = GameManager(c_game, args, config, logger)
     if args.option == "mac":
+        if not isinstance(c_game, list):
+            logger.critical("You are trying to configure multiple games but only one name provided: %s" % (c_game))
+            sys.exit(1)
+        # TODO: Put this as an internal function of game manager
         for game in c_game:
             g_manager = GameManager(game, args, config, logger)
             g_manager.set_self_values(env_dict)
@@ -22,14 +27,39 @@ def run_main(args, config, logger):
         logger.info("Finished configuring all games")
     else:
         if not isinstance(c_game, list):
-            g_manager = GameManager(c_game, args, config, logger)
             g_manager.set_self_values(env_dict)
             if args.option == "p":
                 logger.info("Running game %s\n" % (c_game))
-                time.sleep(1)
                 has_ran = g_manager.run_game_cmd()
                 if has_ran:
-                    sys.exit(0)
+                    if args.resume:
+                        logger.info("Resuming states from scratch might cause unexpected issues please be carefull\n")
+                        time.sleep(1)
+                        state = None
+                        wait_t = None
+                        try:
+                            state = int(args.resume)
+                            if state > 9:
+                                raise ValueError
+                        except ValueError as e:
+                            logger.critical("State must be a number from 0 to 9 killing process...\n")
+                            g_manager.r_proc.terminate()
+                            sys.exit(1)
+                        if args.load_time:
+                            try:
+                                wait_t = int(args.load_time)
+                                if wait_t < 4:
+                                    raise ValueError
+                            except Exception as e:
+                                logger.critical("Wait time must be greater than 4 killing process...\n")
+                                g_manager.r_proc.terminate()
+                                sys.exit(1)
+                            logger.info("Trying to resume state %s" % (state))
+                        else:
+                            wait_t = 15
+                        state_runner(logger, state, wait_t)
+                    else:
+                        sys.exit(0)
                 else:
                     sys.exit(1)
             elif args.option == "ac":
@@ -50,12 +80,12 @@ def start_main():
     sys.stdout.write("\n")
     arg_parser = argparse.ArgumentParser(description='Manage PCSX2 configs')
     args = add_args(arg_parser)
-    di_level = "info" if not args.debug_level else args.debug_level
+    di_level = "info" if not args.debug else args.debug
     d_level = None
     try:
         d_level = getattr(logging, di_level.upper())
     except Exception as e:
-        sys.stdout.write("Invalid log level " + args.debug_level + "\n")
+        sys.stdout.write("Invalid log level " + args.debug + "\n")
         sys.exit(1)
     if not isinstance(d_level, int):
         sys.stdout.write("Invalid log level " + di_level + "\n")
--- Manager/manager_utils.py
+++ Manager/manager_utils.py
@@ -1,9 +1,11 @@
-import sys
+import sys, time
+from pywinauto.application import Application
 
 ## Get user input for action to manage game
 def get_manage_option(again=False):
     if not again:
-        sys.stdout.write("\nSelect the actions by order if creating a new game\n \n")
+        sys.stdout.write("\nSelect the actions by order if creating a new game\n")
+        sys.stdout.write("Creating a symbolic link is unnecessary if you are replacing memory cards path in the config\n\n")
     sys.stdout.write("cf : Copy files from pcsx folder to game dir\n")
     sys.stdout.write("at : Add templates to game dir, rename them, and replace values\n")
     sys.stdout.write("sm : Creates a symbolic link to shared memcards\n")
@@ -23,13 +25,65 @@ def envs_to_dict(config, logger):
 
 ## Add arguments to parser and return them
 def add_args(parser):
-    ## Add group
-    required_g = parser.add_argument_group("Required args")
     # required
-    required_g.add_argument("-cfg", help="Configuration file", action="store", required=True)
-    required_g.add_argument("-option", help="(ac|mac|m|p) ac for auto create game, mac for multiple auto creation, m for manage, p for play", action="store", required=True)
+    parser.add_argument("cfg", help="Configuration file", action="store")
+    parser.add_argument("option", help="(ac|mac|m|p) ac for auto create game, mac for multiple auto creation, m for manage, p for play", action="store")
     # optional
-    parser.add_argument("-debug", "--debug_level", help="Set debug level", action="store")
+    parser.add_argument("--debug", help="Set debug level", action="store")
     parser.add_argument("--game", help="Game name, can be set in manager.ini", action="store")
+    parser.add_argument("--resume", help="Resume state (0 to 9)", action="store")
+    parser.add_argument("--load_time", help="Time in seconds to wait while loading game when resuming state", action="store")
     args = parser.parse_args()
+    games = None
+    if args.game:
+        games = args.game.split(" ") if " " in args.game else args.game
+    if args.resume and args.option != "p":
+        sys.stdout.write("Found resume argument but invalid option %s\n" % (args.option))
+        sys.stdout.write("You can't resume a state while managing or configuring game\n")
+        sys.exit(1)
+    elif args.option == "mac" and args.game and not isinstance(games, list):
+        sys.stdout.write("You tried to configure multiple games but only provided one name: %s\n" % (games))
     return args
+
+# For Initializing the game and then try to resume state
+def state_runner(logger, slot, wait_t):
+    # string for slot regex
+    slot_re = "Slot %s - .*" % (slot)
+    t_wait_t = wait_t
+    wait_t //= 2
+    # Define app and connect
+    logger.info("Connecting to app\n")
+    app = Application(backend="uia").connect(title_re="PCSX2 .*", found_index=0)
+    # Select main window
+    app_window = app.window(title_re="PCSX2 .*")
+    sys_menu_item = app_window[u"SystemMenuItem"]
+    # Click system menu
+    logger.info("Opening system menu\n")
+    sys_menu_item.click_input()
+    sys_child = app_window.child_window(title_re="System", control_type="Menu", found_index=0)
+    # Select boot button trough new child window after selecting menu
+    logger.info("Booting game to load stuff\n")
+    boot = sys_child.child_window(title="Boot ISO (fast)", control_type="MenuItem")
+    boot.click_input()
+    # Wait a bit for the window to load
+    logger.info("Waiting %s seconds for game to load\n" % (t_wait_t))
+    time.sleep(wait_t)
+    # Select game window
+    game_window = app.window(title_re="Slot: .*")
+    # Wait a bit for game to load and close window
+    time.sleep(wait_t)
+    logger.info("Closing game window\n")
+    game_window.close()
+    # Open system menu again
+    logger.info("Reopening menus to load state\n")
+    sys_menu_item.click_input()
+    # Select and open load state menus
+    load_state = sys_child.child_window(title_re="Load state\s.*", control_type="MenuItem")
+    load_state.click_input()
+    # Select state button and click it
+    load_state_child = app_window.child_window(title_re="Load state\s.*", control_type="Menu", found_index=0)
+    l_state_btn = load_state_child.child_window(title_re=slot_re, control_type="MenuItem")
+    logger.info("Loading state\n")
+    l_state_btn.click_input()
+    logger.info("Done, exiting cli\n")
+    sys.exit(0)
